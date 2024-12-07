b4ca15c5ce63d1aaabacb2afa6d3f7a8ef8383c4
Evandro Araújo <evandroqa@gmail.com>
2024-12-06 23:27:36 -0300

    Muitos-para-muitos com classe de associação

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,13 +32,15 @@ public class Product {
 
 	// O JoinColumns será da classe onde estou partindo - neste caso product
 	// Outro lado da tabela de relacionamento - category
-	// Usa o set para não haver repetição na tabela "tb_product_category"
-	// que será criada no ManyToMany	joinColumns = @JoinColumn(name = "product_id"),
-	
+	// Usa o set<> para não haver repetição na tabela "tb_product_category"
+	// que será criada no ManyToMany joinColumns = @JoinColumn(name = "product_id"),
 	@ManyToMany
 	@JoinTable(name = "tb_product_category",
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
