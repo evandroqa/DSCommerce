30c3b670c6579b87381a7ae35d18636e4b09b6bb
Evandro Araújo <evandroqa@gmail.com>
2024-12-09 21:20:09 -0300

    "Database Seeding"

--- src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -20,15 +20,15 @@ public class Category {
 	private String name;
 	
 	@ManyToMany(mappedBy = "categories")
-	private Set<Product> products = new HashSet<>();
+	private Set<Product> product = new HashSet<>();
 
 	public Category() {
 	}
 
-	public Category(Long id, String name, Set<Product> products) {
+	public Category(Long id, String name, Set<Product> product) {
 		this.id = id;
 		this.name = name;
-		this.products = products;
+		this.product = product;
 	}
 
 	public Long getId() {
@@ -47,8 +47,8 @@ public class Category {
 		this.name = name;
 	}
 
-	public void setProducts(Set<Product> products) {
-		this.products = products;
+	public void setProducts(Set<Product> product) {
+		this.product = product;
 	}
 	
 }
