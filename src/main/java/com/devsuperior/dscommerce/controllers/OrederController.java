36de03e6e3cbd60d30ccc38392cbd866c311e249
Evandro Araújo <evandroqa@gmail.com>
2026-03-05 20:55:41 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrederController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrederController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
+		// A exceção é lançada e o ControllerExceptionHandler captura a exception
+		OrderDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/dscommerce/dto/ClientDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.User;
+
+public class ClientDTO {
+	
+	private Long id;
+	private String nome;
+	
+	public ClientDTO(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public ClientDTO(User entity) {
+		id = entity.getId();
+		nome = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}	
+	
+}
